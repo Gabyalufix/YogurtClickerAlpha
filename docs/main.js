74dc5d299057d7f88cfa3b436be747c13e3dbc2a
Gabyalufix <stephen.hartley.jm@gmail.com>
2019-01-16 01:01:03 -0500

    v0.0.9
    Implemented sphere construction/deconstruction UI and tracking.
    Implemented tick structure. Implemented generalized percentage slider
    functions. Added ontouchstart event listener (touchscreen support).

--- docs/main.js
+++ docs/main.js
@@ -45,11 +45,20 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// PERCENT SLIDERS:
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 function onInputMultiSliderPct(ss){
 	  var cval = parseFloat(ss.value)
 	  if(ss.ktotal + cval >= 10000){
 	    ss.value = 10000 - ss.ktotal
-		for(j = 0; j < 3; j++){
+		for(j = 0; j < ss.othrArray.length; j++){
 		  if(ss.othrArray[j].lockbox.checked == false){
 		    ss.othrArray[j].value = 0
 		  }
@@ -57,7 +66,7 @@ function onInputMultiSliderPct(ss){
 		}
 	  } else if(ss.ktotal + cval < 10000 && ss.stotal == 0) {
 	    ss.value = 10000 - ss.ktotal
-		for(j = 0; j < 3; j++){
+		for(j = 0; j < ss.othrArray.length; j++){
 		  if(ss.othrArray[j].lockbox.checked == false){
 		    ss.othrArray[j].value = 1
 		  }
@@ -66,23 +75,23 @@ function onInputMultiSliderPct(ss){
 		onDownMultiSliderPct(ss)
 	  } else {
 	    var othrVal = 10000 - ss.ktotal - cval
-        for(j = 0; j < 3; j++){
+        for(j = 0; j < ss.othrArray.length; j++){
           if(ss.othrArray[j].lockbox.checked == false){
             ss.othrArray[j].value = ss.ssf[j] * othrVal
           }
           ss.othrArray[j].sdisplay.innerHTML = ss.othrArray[j].value / 100
         }
-	    output.innerHTML = ss.stotal / 100
+	    //output.innerHTML = ss.stotal / 100
 	  }
 	  ss.sdisplay.innerHTML = parseFloat(ss.value) / 100
 }
 
 function onDownMultiSliderPct(ss){
-	  output.innerHTML = "DOWN"
+	  //output.innerHTML = "DOWN"
       ss.stotal = 0
       ss.ktotal = 0
       ss.downval = parseFloat(ss.value);
-      for(j = 0; j < 3; j++){
+      for(j = 0; j < ss.othrArray.length; j++){
         if(ss.othrArray[j].lockbox.checked == true){
           ss.ssf[j] = parseFloat(ss.othrArray[j].value);
           ss.ktotal = ss.ktotal + ss.ssf[j]
@@ -91,170 +100,56 @@ function onDownMultiSliderPct(ss){
           ss.stotal = ss.stotal + ss.ssf[j]
         }
       }
-      for(j = 0; j < 3; j++){
+      for(j = 0; j < ss.othrArray.length; j++){
         ss.ssf[j] = ss.ssf[j] / ss.stotal
       }
 }
 
-var output = document.getElementById("bioSliderPctVal");
-output.innerHTML = 10;
+//var output = document.getElementById("bioSliderPctVal");
+//output.innerHTML = 10;
 
 
-var SCIENCE_FIELDS = ["bio","eng","psy"]
-var SCIENCE_SUBFIELDCT = [3,3,3]
+var PCTSLIDER_FIELDS = ["bio","eng","psy","bot","green"]
+var PCTSLIDER_SUBFIELDCT = [4,4,4,4,3]
+var PCTSLIDERS = {}
 
-for(sfi = 0; sfi < SCIENCE_FIELDS.length; sfi++){
-   var slider_NAMES = [];
-   
-}
-
-var eng_slider_NAMES = ["engSliderPct1","engSliderPct2","engSliderPct3","engSliderPct4"]
-var eng_check_NAMES = ["engSliderCheck1","engSliderCheck2","engSliderCheck3","engSliderCheck4"]
-var eng_display_NAMES = ["engSliderDisplayPct1","engSliderDisplayPct2","engSliderDisplayPct3","engSliderDisplayPct4"]
-var eng_check_elem = []
-var eng_display_elem = []
-var eng_slider_elem = []
-for(i = 0; i < 4; i++){
-     eng_slider_elem[i] = document.getElementById(eng_slider_NAMES[i]);
-     eng_check_elem[i] = document.getElementById(eng_check_NAMES[i]);
-     eng_display_elem[i] = document.getElementById(eng_display_NAMES[i]);
-}
-
-for(i = 0; i < 4; i++){
-    var ss = eng_slider_elem[i];
-    ss.sdisplay = eng_display_elem[i]
-    ss.othrArray = eng_slider_elem.slice()
-    ss.othrArray.splice(i,1)
-    ss.lockbox = eng_check_elem[i]
-    ss.ssf = []
-    ss.stotal = 0
-	ss.onmousedown = function() {
-	  onDownMultiSliderPct(this)
+for(sfi = 0; sfi < PCTSLIDER_FIELDS.length; sfi++){
+    var fid = PCTSLIDER_FIELDS[sfi]
+    var fct = PCTSLIDER_SUBFIELDCT[sfi]
+	var check_elem = []
+	var display_elem = []
+	var slider_elem = []
+	for(i = 0; i < fct; i++){
+	     console.log("1: "+fid+"SliderCheck"+(i+1))
+	     slider_elem[i] = document.getElementById(fid+"SliderPct"+(i+1));
+	     check_elem[i] = document.getElementById(fid+"SliderCheck"+(i+1));
+	     display_elem[i] = document.getElementById(fid+"SliderDisplayPct"+(i+1));
+	     console.log("2: "+fid+"SliderCheck"+(i+1))
 	}
-	ss.onmouseup = function() {
-	  output.innerHTML = "UP"
-	}
-	ss.oninput = function() {
-      onInputMultiSliderPct(this)
-	}
-	ss.onchange = function() {
-      onInputMultiSliderPct(this)
-	}
-}
-
-
-
-
-
-
-
-var s1 = document.getElementById("bioSliderPct1");
-var s2 = document.getElementById("bioSliderPct2");
-var s3 = document.getElementById("bioSliderPct3");
-var s4 = document.getElementById("bioSliderPct4");
-var sarray = [s1,s2,s3,s4];
-var scbaNAMES = ["bioSliderCheck1","bioSliderCheck2","bioSliderCheck3","bioSliderCheck4"]
-var sdisplayNAMES = ["bioSliderDisplayPct1","bioSliderDisplayPct2","bioSliderDisplayPct3","bioSliderDisplayPct4"]
-var scba = []
-var sdisplay = []
-for(i = 0; i < 4; i++){
-   scba[i] = document.getElementById(scbaNAMES[i]);
-   sdisplay[i] = document.getElementById(sdisplayNAMES[i])
-}
-
-for(i = 0; i < 4; i++){
-    var ss = sarray[i];
-    console.log("i = "+i)
-    ss.sdisplay = sdisplay[i]
-    ss.othrArray = sarray.slice()
-    ss.othrArray.splice(i,1)
-    ss.lockbox = scba[i]
-    ss.ssf = []
-    ss.stotal = 0
-	ss.onmousedown = function() {
-	  onDownMultiSliderPct(this)
-	}
-	ss.ontouchstart = function() {
-	  onDownMultiSliderPct(this)
-	}
-	ss.onmouseup = function() {
-	  output.innerHTML = "UP"
-	}
-	ss.oninput = function() {
-      onInputMultiSliderPct(this)
-	}
-	ss.onchange = function() {
-      onInputMultiSliderPct(this)
-	}
-	
-}
-
-var eng_slider_NAMES = ["engSliderPct1","engSliderPct2","engSliderPct3","engSliderPct4"]
-var eng_check_NAMES = ["engSliderCheck1","engSliderCheck2","engSliderCheck3","engSliderCheck4"]
-var eng_display_NAMES = ["engSliderDisplayPct1","engSliderDisplayPct2","engSliderDisplayPct3","engSliderDisplayPct4"]
-var eng_check_elem = []
-var eng_display_elem = []
-var eng_slider_elem = []
-for(i = 0; i < 4; i++){
-     eng_slider_elem[i] = document.getElementById(eng_slider_NAMES[i]);
-     eng_check_elem[i] = document.getElementById(eng_check_NAMES[i]);
-     eng_display_elem[i] = document.getElementById(eng_display_NAMES[i]);
-}
-
-for(i = 0; i < 4; i++){
-    var ss = eng_slider_elem[i];
-    ss.sdisplay = eng_display_elem[i]
-    ss.othrArray = eng_slider_elem.slice()
-    ss.othrArray.splice(i,1)
-    ss.lockbox = eng_check_elem[i]
-    ss.ssf = []
-    ss.stotal = 0
-	ss.onmousedown = function() {
-	  onDownMultiSliderPct(this)
-	}
-	ss.onmouseup = function() {
-	  output.innerHTML = "UP"
-	}
-	ss.oninput = function() {
-      onInputMultiSliderPct(this)
-	}
-	ss.onchange = function() {
-      onInputMultiSliderPct(this)
-	}
-}
-
-
-var psy_slider_NAMES = ["psySliderPct1","psySliderPct2","psySliderPct3","psySliderPct4"]
-var psy_check_NAMES = ["psySliderCheck1","psySliderCheck2","psySliderCheck3","psySliderCheck4"]
-var psy_display_NAMES = ["psySliderDisplayPct1","psySliderDisplayPct2","psySliderDisplayPct3","psySliderDisplayPct4"]
-var psy_check_elem = []
-var psy_display_elem = []
-var psy_slider_elem = []
-for(i = 0; i < 4; i++){
-     psy_slider_elem[i] = document.getElementById(psy_slider_NAMES[i]);
-     psy_check_elem[i] = document.getElementById(psy_check_NAMES[i]);
-     psy_display_elem[i] = document.getElementById(psy_display_NAMES[i]);
-}
-
-for(i = 0; i < 4; i++){
-    var ss = psy_slider_elem[i];
-    ss.sdisplay = psy_display_elem[i]
-    ss.othrArray = psy_slider_elem.slice()
-    ss.othrArray.splice(i,1)
-    ss.lockbox = psy_check_elem[i]
-    ss.ssf = []
-    ss.stotal = 0
-	ss.onmousedown = function() {
-	  onDownMultiSliderPct(this)
-	}
-	ss.onmouseup = function() {
-	  output.innerHTML = "UP"
-	}
-	ss.oninput = function() {
-      onInputMultiSliderPct(this)
-	}
-	ss.onchange = function() {
-      onInputMultiSliderPct(this)
+	PCTSLIDERS[fid] = {checkElem: check_elem, displayElem: display_elem, sliderElem:slider_elem}
+	for(i = 0; i < fct; i++){
+		var ss = slider_elem[i];
+		ss.sdisplay = display_elem[i]
+		ss.othrArray = slider_elem.slice()
+		ss.othrArray.splice(i,1)
+		ss.lockbox = check_elem[i]
+		ss.ssf = []
+		ss.stotal = 0
+		ss.onmousedown = function() {
+		  onDownMultiSliderPct(this)
+		}
+		ss.ontouchstart = function() {
+		  onDownMultiSliderPct(this)
+		}
+		//ss.onmouseup = function() {
+		  //output.innerHTML = "UP"
+		//}
+		ss.oninput = function() {
+		  onInputMultiSliderPct(this)
+		}
+		ss.onchange = function() {
+		  onInputMultiSliderPct(this)
+		}
 	}
 }
 
@@ -263,24 +158,99 @@ for(i = 0; i < 4; i++){
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+SETTINGS = {ADD_MULTIPLIER:{}}
+INVENTORY = {}
+CONSTRUCTION_BUFFER = {WORLDS_CONST_CT:{},WORLDS_DECON_CT:{}}
+CONSTRUCTION_BUFFER["WORLDS_CONST"] = {}
+CONSTRUCTION_BUFFER["WORLDS_DECON"] = {}
+
+STATS = {WORLD_BUILD_TIME:{Fallow:0,Pop:0,Omni:5000, Bot:20000, Green:10000, Comp:2000, Hub:2000}}
+STATS["WORLD_DECON_TIME"]={Fallow:0,Pop:0,Omni:5000, Bot:20000, Green:10000, Comp:2000, Hub:2000}
 
 WORLD_TYPE_LIST = ["Fallow","Pop","Omni","Bot","Green","Comp","Hub"]
+WORLD_TYPE_DYSON = [false,false,true,true,true,true,true]
 DYSON_TYPE_LIST = ["Omni","Bot","Green","Comp","Hub"]
-INVENTORY = {}
 
 for( i = 0; i < WORLD_TYPE_LIST.length; i++){
-	INVENTORY[WORLD_TYPE_LIST[i] + "_CT"] = 1
+    worldType = WORLD_TYPE_LIST[i]
+	INVENTORY[worldType] = {CT:1}
+	SETTINGS["ADD_MULTIPLIER"][worldType] = 1
+	CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] = 0
+	CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] = 0
+	CONSTRUCTION_BUFFER["WORLDS_CONST"][worldType] = []
+	CONSTRUCTION_BUFFER["WORLDS_DECON"][worldType] = []
 }
 
 for( i = 0; i < DYSON_TYPE_LIST.length; i++){
-
+   worldType = DYSON_TYPE_LIST[i]
+   multDisplay = document.getElementById(""+worldType+"_wf_AddUnit")
+   multUp = document.getElementById("button_wf"+worldType+"UP")
+   multDn = document.getElementById("button_wf"+worldType+"DN")
+   multUp.disp = multDisplay
+   multDn.disp = multDisplay
+   multUp.worldType = worldType
+   multDn.worldType = worldType
+   multDn.mdn = multDn
+   multUp.mdn = multDn
+   
+   multDn.disabled = true;
+   multUp.onclick = function(){
+     SETTINGS["ADD_MULTIPLIER"][this.worldType] = Math.round(SETTINGS["ADD_MULTIPLIER"][this.worldType] * 1000)
+     this.disp.innerHTML = getPrefixSI( SETTINGS["ADD_MULTIPLIER"][this.worldType] )
+     this.mdn.disabled = false;
+   }
+   multDn.onclick = function(){
+     SETTINGS["ADD_MULTIPLIER"][this.worldType] = Math.round(SETTINGS["ADD_MULTIPLIER"][this.worldType] / 1000)
+     this.disp.innerHTML = getPrefixSI( SETTINGS["ADD_MULTIPLIER"][this.worldType] )
+     if(SETTINGS["ADD_MULTIPLIER"][this.worldType] <= 1){
+       this.mdn.disabled = true;
+     }
+   }
+   addButtonList = ["BB","B","F","FF"]
+   addButtonMult = [10,1,1,10]
+   addButtonPos  = [false,false,true,true]
+   for(j=0;j < addButtonList.length;j++){
+     bname = addButtonList[j]
+     butelem = document.getElementById("button_wf"+worldType+bname)
+     butelem.worldType = worldType;
+     butelem.addMult = addButtonMult[j]
+     butelem.addPositive = addButtonPos[j]
+     butelem.onclick = function(){
+       if(this.addPositive){
+         startWorldConstruction(this.worldType,this.addMult * SETTINGS["ADD_MULTIPLIER"][this.worldType])
+       } else {
+         startWorldDeconstruction(this.worldType,this.addMult * SETTINGS["ADD_MULTIPLIER"][this.worldType])
+       }
+     }
+   }
+   
 }
 
+//			<h5>OmniWorlds: <span id="Omni_CT"></span></h5>
+//			   <div class="buildCtrlPanel">
+//				<button class = "button2" id ="button_wfOmniBB" > -- </button>
+//				<button class = "button2" id ="button_wfOmniB" > - </button>
+//				<button class = "button2" id ="button_wfOmniF" > + </button>
+//				<button class = "button2" id ="button_wfOmniFF" > ++ </button>
+//				<button class = "button3" id ="button_wfOmniUP" > &#8963; </button>
+//			   <div class="buildInfoPanel1">kS</div>
+//			   <div class="buildInfoPanel2">BEEP BEEP BEEP</div>
+//				<button class = "button3" id ="button_wfOmniDN" > &#8964; </button>
+//			   </div>
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///MAIN TICK CYCLE:
+
+var TICK_TIMESTAMP
 var secTimer = 0
 window.setInterval(function(){
-
+    TICK_TIMESTAMP = Date.now()
+    TICK_readUserInputs()
     TICK_captureSystems()
+    TICK_constructWorlds()
 	TICK_updateWorldCounts()
 
     secTimer++;
@@ -296,15 +266,100 @@ function calculateSlowTick(){
 	varNumTicksSoFar = varNumTicksSoFar + 1
 	printlnToAiConsole("" + varNumTicksSoFar+" secTimer = "+secTimer)
 }
+//SETTINGS["bio"+"_FRACTION"]
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//TICK FUNCTIONS:
+
+
+function TICK_readUserInputs(){
+  //Percent sliders:
+  for(i =0; i<PCTSLIDER_FIELDS.length; i++){
+    fid = PCTSLIDER_FIELDS[i]
+    SETTINGS[fid+"_FRACTION"] = [];
+    for(j=0;j<PCTSLIDERS[fid]["displayElem"].length;j++){
+      SETTINGS[fid+"_FRACTION"][j] = parseFloat( PCTSLIDERS[fid]["sliderElem"][j].value ) / 10000
+    }
+  }
+  
+}
 function TICK_updateWorldCounts(){
 	for( i = 0; i < WORLD_TYPE_LIST.length; i++){
-  		document.getElementById(WORLD_TYPE_LIST[i] + "_CT").innerHTML = fmtSI(INVENTORY[WORLD_TYPE_LIST[i] + "_CT"])
+	    worldType = WORLD_TYPE_LIST[i]
+	    worldCountLine = fmtSI(INVENTORY[WORLD_TYPE_LIST[i]]["CT"])
+  		if( CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] > 0 ){
+  		  worldCountLine = worldCountLine + " (building: "+fmtSI(CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType])+")"
+  		}
+  		if( CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] > 0 ){
+  		  worldCountLine = worldCountLine + " (breaking: "+fmtSI(CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType])+")"
+  		}
+  		document.getElementById(WORLD_TYPE_LIST[i] + "_CT").innerHTML = worldCountLine
 	}
 }
 function TICK_captureSystems(){
-    INVENTORY["Fallow_CT"] = ( INVENTORY["Fallow_CT"] * ( 1 + Math.random()/2500 ))
+    INVENTORY["Fallow"]["CT"] = ( INVENTORY["Fallow"]["CT"] * ( 1 + Math.random()/2500 ))
 }
+function TICK_constructWorlds(){
+  
+  for(i=0;i<DYSON_TYPE_LIST.length;i++){
+    worldType = DYSON_TYPE_LIST[i]
+    if(CONSTRUCTION_BUFFER["WORLDS_CONST"][worldType].length > 0){
+      if(  CONSTRUCTION_BUFFER["WORLDS_CONST"][worldType][0][1] < Date.now() ){
+        nxt = CONSTRUCTION_BUFFER["WORLDS_CONST"][worldType].shift()
+        INVENTORY[worldType]["CT"] = INVENTORY[worldType]["CT"] + nxt[0]
+        CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] = CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] - nxt[0]
+      }
+    }
+    if(CONSTRUCTION_BUFFER["WORLDS_DECON"][worldType].length > 0){
+      if(  CONSTRUCTION_BUFFER["WORLDS_DECON"][worldType][0][1] < Date.now() ){
+        nxt = CONSTRUCTION_BUFFER["WORLDS_DECON"][worldType].shift()
+        INVENTORY[worldType]["CT"] = INVENTORY[worldType]["CT"] - nxt[0]
+        CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] = CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] - nxt[0]
+      }
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+function startWorldConstruction(worldType,batchCt){
+  CONSTRUCTION_BUFFER["WORLDS_CONST"][worldType].push([batchCt, (Date.now() + STATS["WORLD_BUILD_TIME"][worldType]) ])
+  CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] = CONSTRUCTION_BUFFER["WORLDS_CONST_CT"][worldType] + batchCt
+}
+function startWorldDeconstruction(worldType,batchCt){
+  if(INVENTORY[worldType]["CT"] - CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] < batchCt){
+    //console.log("deconstructing ALL: "+batchCt+">"+INVENTORY[worldType]["CT"]);
+    startWorldDeconstruction(worldType,INVENTORY[worldType]["CT"] - CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType])
+  } else {
+    //console.log("deconstructing Some: "+batchCt+"<="+INVENTORY[worldType]["CT"]);
+    CONSTRUCTION_BUFFER["WORLDS_DECON"][worldType].push([batchCt, (Date.now() + STATS["WORLD_DECON_TIME"][worldType]) ])
+    CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] = CONSTRUCTION_BUFFER["WORLDS_DECON_CT"][worldType] + batchCt
+  }
+}
+
+//startWorldConstruction("Green",100)
+//CONSTRUCTION_BUFFER["WORLDS_CONST"]["Green"]
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
 
 AI_CONSOLE_AUTOSCROLL=true
 AI_CONSOLE_HTMOD=0
@@ -340,7 +395,7 @@ function fmtSI(x){
   if(x < 100){
     return roundTo(x,1)
   } else if(x < 1000){
-    return ""+round(x)
+    return ""+Math.round(x)
   }
   d = Math.floor(Math.log10(x))
   dd = Math.floor(d / 3) - 1
@@ -352,3 +407,14 @@ function fmtSI(x){
   return roundTo(rr,dp) + suffix
 }
 
+function getPrefixSI(x){
+  if(x < 1000){
+    return ""
+  }
+  d = Math.floor(Math.log10(x))
+  dd = Math.floor(d / 3) - 1
+  ddd = Math.floor(dd / 8)
+  ddp = dd - ddd * 8
+  prefix = SIPREFIX[ddp] + "Y".repeat(ddd)
+  return prefix
+}
